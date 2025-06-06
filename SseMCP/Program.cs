7af1f87f3a7ebd48c7bb5a2c62ecadd0eac72e5a
Kasper1985 <Kasper1985@gmail.com>
2025-06-06 14:21:09 +0200

    added SSE MCP server

---
+++ SseMCP/Program.cs
@@ -0,0 +1,15 @@
+﻿using SseMCP;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddMcpServer()
+    .WithHttpTransport()
+    .WithTools<TimeTool>();
+
+var app = builder.Build();
+
+app.MapMcp();
+
+await app.RunAsync("http://localhost:5001");
